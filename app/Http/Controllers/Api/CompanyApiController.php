81a61e91e266dd1d37410ea3eecf90f6e79d7afa
bayram61 <bayramyilmaz061@gmail.com>
2021-07-02 17:14:52 +0300

    nba

--- app/Http/Controllers/Api/CompanyApiController.php
+++ app/Http/Controllers/Api/CompanyApiController.php
@@ -10,34 +10,41 @@ class CompanyApiController extends Controller
 {
     public function getCompanies()  
     {
-        $query=DB::table('companies')
-            ->select(
-                'id',
-                'company_name',
-                'company_owner',
-                'company_email',
-                'company_phone',
-                'company_status',
-                'created_at'
-            );
-        
-        return datatables($query)
-            ->addColumn('action',function($company){
-                return '
-                <div class="btn-group " >
-                <a href="'.route('companies.show', $company->id).'" class="btn  btn-outline-secondary">
-                <i class="bi bi-arrows-fullscreen"></i>
-                </a>
-                <a href="'.route('companies.edit',$company->id).'" class="btn  btn-outline-secondary">
-                <i class="bi bi-pencil-square"></i>
-                </a>
-                <a href="'.route('companies.delete',$company->id).'" class="btn  btn-outline-secondary">
-                <i class="bi bi-trash"></i>
-                </a>
-                </div>
-                ';
-            })->make(true);
+        $query=DB::table('companies')->all();
+           return response->json($query);
 
         
     }
+    // public function getCompanies()  
+    // {
+    //     $query=DB::table('companies')
+    //         ->select(
+    //             'id',
+    //             'company_name',
+    //             'company_owner',
+    //             'company_email',
+    //             'company_phone',
+    //             'company_status',
+    //             'created_at'
+    //         );
+        
+    //     return datatables($query)
+    //         ->addColumn('action',function($company){
+    //             return '
+    //             <div class="btn-group " >
+    //             <a href="'.route('companies.show', $company->id).'" class="btn  btn-outline-secondary">
+    //             <i class="bi bi-arrows-fullscreen"></i>
+    //             </a>
+    //             <a href="'.route('companies.edit',$company->id).'" class="btn  btn-outline-secondary">
+    //             <i class="bi bi-pencil-square"></i>
+    //             </a>
+    //             <a href="'.route('companies.delete',$company->id).'" class="btn  btn-outline-secondary">
+    //             <i class="bi bi-trash"></i>
+    //             </a>
+    //             </div>
+    //             ';
+    //         })->make(true);
+
+        
+    // }
 }
