ca58a00cbc711ce77bc82dd3b88ddbe0974ac122
bayram61 <bayramyilmaz061@gmail.com>
2021-07-04 17:08:01 +0300

    deneme

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CompanyFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Company::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'company_name'=>$this->faker->name(),
+            'company_owner'=>$this->faker->name(),
+            'company_email'=>'bayram@bayram.com',
+            'company_phone'=>'05386198724',
+            'company_status'=>'Aktif',
+            'users_id'=>1,
+
+
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
            // PersonTableSeeder::class,
            // ProjectTableSeeder::class,
-            EvaluationTableSeeder::class,
+           // EvaluationTableSeeder::class,
+            CompanyTableSeeder::class,
         ]);
     }
 }
